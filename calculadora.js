b5f85cfd03ff25c88d126670b2944cf8a759506e
maci <estermarrimaxiel@gmail.com>
2024-03-27 09:50:42 -0300

    calculadora

---
+++ calculadora.js
@@ -0,0 +1,6 @@
+function calcularsoma(){
+    var num1soma = document.getAnimations.EleementById('num1soma'). value;
+    var num2soma = document.getAnimations.EleementById('num2soma'). value;
+    num1soma = parseFloat(num1soma);
+    
+}
