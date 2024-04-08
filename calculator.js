52c9b38bae258ebb7baff8076ef312d1ee0a764a
maci <estermarrimaxiel@gmail.com>
2024-04-08 10:55:51 -0300

    correto 2.0

--- calculator.js
+++ calculator.js
@@ -5,5 +5,10 @@ function clearDisplay(){
     document.getElementById('display').value = '';
 }
 function calcular(){
-    var expressao = document.getElementById
+    var expressao = document.getElementById('display').value;
+    var operandos = expressao.split(/[+\-*\/]/);
+    var operador = expressao.match(/[+\-*\/]/);
+    if (operandos.length !==2 || !operador) {
+        
+    }
 }
