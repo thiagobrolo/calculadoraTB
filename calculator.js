99c5d8470225821e27f6037e34988bc3e4117655
maci <estermarrimaxiel@gmail.com>
2024-04-08 09:55:50 -0300

    correta

---
+++ calculator.js
@@ -0,0 +1,9 @@
+function addTodisplay(value) {
+    document.getElementById('display').value += value;
+}
+function clearDisplay(){
+    document.getElementById('display').value = '';
+}
+function calcular(){
+    var expressao = document.getElementById
+}
